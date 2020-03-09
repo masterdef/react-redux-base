2c5384005b7fc835fa68096d30392107ed7b101e
Andrew <andrewrr@Andrejs-MacBook-Air.local>
2020-03-09 16:40:09 +0200

    upd

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
-export const fetchPosts = async () => {
-  const response = await jsonPlaceholder.get('/posts');
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
 
-  return {
-    type: 'FETCH_POSTS',
-    payload: response
-  }
+    dispatch({
+      type: 'FETCH_POSTS',
+      payload: response
+    });
 }
